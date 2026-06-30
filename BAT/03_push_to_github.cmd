4d3ffa1279833d89176e3b7215a0da04d3869fb9
Gink Hsu [許騰元] <gink.hsu@sti.com.tw>
2026-06-30 13:22:28 +0800

    -

--- BAT/03_push_to_github.cmd
+++ BAT/03_push_to_github.cmd
@@ -1,12 +1,24 @@
 @echo off
+chcp 65001 >nul
 cd /d E:\Programs\GitHub\Scoring-System
+
+set VERSION=20260630-0050
+set CHANGE_SUMMARY=新增學生出生年並優先用於 PR 年齡估算
+set COMMIT_MESSAGE=%VERSION% - %CHANGE_SUMMARY%
 
 git status
 
 git add .
 
-set /p msg=Enter commit message:
-git commit -m "%msg%"
+git diff --cached --quiet
+if %errorlevel%==0 (
+  echo No changes to commit.
+  pause
+  exit /b 0
+)
+
+echo Commit message: %COMMIT_MESSAGE%
+git commit -m "%COMMIT_MESSAGE%"
 
 git push origin main
 
